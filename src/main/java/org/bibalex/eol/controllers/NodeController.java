b37ca23d7ae38431046adcf697d115fb39f17c4e
menna-hedaia <menna.hedaia@bibalex.org>
2019-12-05 13:55:19 +0200

    continue_nodes_requests

--- src/main/java/org/bibalex/eol/controllers/NodeController.java
+++ src/main/java/org/bibalex/eol/controllers/NodeController.java
@@ -1,13 +1,17 @@
 package org.bibalex.eol.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bibalex.eol.collections.Node;
 import org.bibalex.eol.repositories.NodeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+
+import javax.swing.text.Document;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -19,15 +23,41 @@ public class NodeController {
     @Autowired
     private NodeRepository nodeRepo;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/getAllNodes", method = RequestMethod.GET)
     public List<Node> getAllNodes() {
         return nodeRepo.findAll();
     }
 
     @RequestMapping(value = "/createNode", method = RequestMethod.POST, consumes = "application/json")
     public Node createNode(@RequestBody Node node) {
-        System.out.println(node.toString());
         return nodeRepo.save(node);
     }
+
+    @RequestMapping(value= "/insertNodes", method = RequestMethod.POST, consumes = "application/json")
+    public void insertNodes (@RequestBody List<Node> nodes) throws IOException {
+//        return  nodeRepo.insert(nodes);
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Java object to JSON file
+        for (Node node: nodes) {
+            mapper.writeValue(new File("nodes.json"), node);
+        }
+
+
+
+    }
+
+    @RequestMapping(value="/findNodesByResource/{resourceId}", method = RequestMethod.GET)
+    public List<Node> findNodesByResource(@PathVariable("resourceId") int resourceId)
+    {
+        return nodeRepo.findByresourceId(resourceId);
+
+    }
+
+
+
+
+
+
 
 }
--- src/main/java/org/bibalex/eol/repositories/NodeRepository.java
+++ src/main/java/org/bibalex/eol/repositories/NodeRepository.java
@@ -3,7 +3,10 @@ package org.bibalex.eol.repositories;
 import org.bibalex.eol.collections.Node;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface NodeRepository extends MongoRepository<Node, String> {
 
-    public Node save(Node node);
+    List<Node> findByresourceId(int resourceId);
+    //List<Node> findByresourceIdAndnodeId(int resourceId, String nodeId);
 }
